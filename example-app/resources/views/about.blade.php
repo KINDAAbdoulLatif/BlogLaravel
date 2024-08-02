edf4ff78910f8901706d973bec324bef5b8be7d0
KINDAAbdoulLatif <146890747+KINDAAbdoulLatif@users.noreply.github.com>
2024-08-02 11:03:30 +0200

    Delete example-app directory

--- example-app/resources/views/about.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout')
-@section('titre', 'page about')
-    
-@endsection
-@section('paragraphe')
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, iste?</p>
-@endsection
--- example-app/resources/views/article.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-<h1>Articles</h1>
-<ul>
-    @foreach ($articles as $article)
-        <li>{{ $article->title }}</li>
-        
-    @endforeach
-</ul>
